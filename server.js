5eeaa1eaf723297a5ae54f188fd8ceadfb4edc45
BossNPtl <boss.dowork@gmail.com>
2024-06-19 15:02:43 +0700

    create dotenv

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
-const express = require('express')
+require('dotenv').config();
+const express = require('express');
+
 const app = express()
 
-app.listen(8000)
+const port = process.env.PORT || 8000
+app.listen(port, () => console.log("Server is running on", port));
